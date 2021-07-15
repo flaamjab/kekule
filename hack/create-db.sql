5953621e916ae791295e7317f52f11c041dca36d
Daniel Delendik <flaamjab@gmail.com>
2021-07-15 21:30:48 +0500

    add the database creation sql

--- hack/create-db.sql
+++ hack/create-db.sql
@@ -1,5 +1,6 @@
 create table Item (
-  sku primary key,
+  id integer primary key,
+  sku text generated always as ("c" || category || "n" || id),
   name string,
   price double,
   category integer,
