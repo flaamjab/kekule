8a913a7c707c1f72910db0286df805351041a149
Daniel Delendik <flaamjab@gmail.com>
2021-07-15 21:31:14 +0500

    add server.go

--- cmd/kekule/server.go
+++ cmd/kekule/server.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/flaamjab/kekule/internal/api"
+import (
+	"fmt"
+	"os"
+
+	"github.com/flaamjab/kekule/internal/api"
+)
 
 func main() {
-	api.Run()
+	if len(os.Args) == 2 {
+		addr := os.Args[1]
+		api.Run(addr)
+	}
+	if len(os.Args) == 1 {
+		fmt.Println("Please provide the adress to listen at (e.g. localhost:8080)")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		os.Exit(1)
+	}
 }
