b64acdd339fc52dfd5d8f0eee991cc4d1bc0d46b
Daniel Delendik <flaamjab@gmail.com>
2021-07-16 16:58:51 +0500

    split an error message across 2 lines

--- cmd/kekule/server.go
+++ cmd/kekule/server.go
@@ -13,7 +13,10 @@ func main() {
 		api.Run(addr)
 	}
 	if len(os.Args) == 1 {
-		fmt.Println("Please provide the adress to listen at (e.g. localhost:8080)")
+		fmt.Println(
+			"Please provide the adress to listen at " +
+				"(e.g. localhost:8080)",
+		)
 		os.Exit(1)
 	} else if len(os.Args) > 2 {
 		fmt.Println("Too many arguments")
